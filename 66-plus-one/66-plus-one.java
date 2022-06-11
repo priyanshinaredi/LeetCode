f55109b0c753ec72f71ab5aa8892576c9d966277
Priyanshi Naredi <66825094+priyanshinaredi@users.noreply.github.com>
2022-06-11 14:05:06 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (46.78%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+            for(int i=digits.length-1;i>=0;i--)
+    {
+        if(digits[i]==9)
+        {
+            digits[i]=0;
+        }
+        else{
+            digits[i]++;
+            return digits;
+        }   
+     }
+    
+     int[] sol= new int[digits.length+1];
+        sol[0]=1;
+     return sol;
+}
+            
+        }
+        
+            
+            
+        
+    
