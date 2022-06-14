8c74397fd068780f46b9bd9db431caacaa7eef21
Priyanshi Naredi <66825094+priyanshinaredi@users.noreply.github.com>
2022-06-14 23:26:51 +0530

    Added solution - LeetHub

---
+++ Balanced Array - GFG/balanced-array.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+//User function Template for Java
+
+
+class Solution
+{
+    long minValueToBalance(long a[] ,int n)
+    {
+        long sumstart=a[0];
+        long sumend =a[n-1];
+       
+       for(int i=1;i<n/2;i++){
+           sumstart+=a[i];
+           sumend+=a[n-(i+1)];
+           
+       }
+       
+        return sumstart>sumend?sumstart-sumend:sumend-sumstart;
+        
+
+    }
+}
+
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main (String[] args) throws IOException{
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while(testcases-- > 0){
+            int n=Integer.parseInt(br.readLine());
+
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long a[]=new long[n];
+            for(int  i=0;i<n;i++)
+            {
+                a[i]=Long.parseLong(a1[i]);
+            }
+            Solution ob=new Solution();
+            
+            long ans=ob.minValueToBalance(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+
+
+  // } Driver Code Ends
