c290babb64cdc03498d793ef822ad2b8380f8751
Priyanshi Naredi <66825094+priyanshinaredi@users.noreply.github.com>
2022-06-15 00:09:19 +0530

    Added solution - LeetHub

---
+++ Minimum product pair - GFG/minimum-product-pair.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.printMinimumProduct(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public long printMinimumProduct(long arr[], long n)
+    {
+        long smallest=arr[0];
+        long secondsmallest=Integer.MAX_VALUE;
+        for(int i=1;i<n;i++){
+            if(arr[i]<smallest){
+                secondsmallest=smallest;
+                smallest=arr[i];
+            }
+            else if(arr[i]<secondsmallest){
+                secondsmallest=arr[i];
+            }
+        }
+        return smallest*secondsmallest;
+    }
+}
